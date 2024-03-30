7c43548be419fc5944b7ee792ca960a6bb3f9732
Akbarkhan <158039916+akesh0805@users.noreply.github.com>
2024-03-30 17:18:23 +0000

    greeting.task finished

--- CSharpModule.Tasks/Greeting.cs
+++ CSharpModule.Tasks/Greeting.cs
@@ -2,5 +2,10 @@ namespace CSharpModule.Tasks;
 
 public class Greeting
 {
-    
+    public string Greet()
+    {
+        Console.WriteLine("Hello World 👋");
+        
+        return "Hello World 👋";
+    }
 }
