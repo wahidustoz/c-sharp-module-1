d7cf911b6f62307a0f55ed0c819067fc86ce7465
Wahid Abduhakimov <wakhid2802@gmail.com>
2024-07-23 01:26:14 +0500

    Tic Tac toe

---
+++ TicTacToe/Components/CardComponent.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TicTacToe.Components;
+
+public partial class CardComponent
+{
+    [Parameter]
+    public string? Data { get; set; }
+    [Parameter]
+    public (int Row, int Column) Position { get; set; }
+    [Parameter]
+    public EventCallback<(int Row, int Column)> OnClicked { get; set; }
+
+    async Task OnCardClicked() => await OnClicked.InvokeAsync(Position);
+}
---
+++ TicTacToe/Pages/Home.razor.cs
@@ -0,0 +1,75 @@
+﻿using System.Data;
+using System.Timers;
+
+namespace TicTacToe.Pages;
+
+public partial class Home
+{
+    // matrix ichiga bosilga X va O larni 1 va 2 sifatida saqlay
+    int[,] matrix = new int[3,3];
+    bool computersTurn = false;
+    bool isUserX = new Random().Next(0, 1) == 0;
+
+    string? TransformIntoData(int data)
+    => data switch 
+    {
+        1 => "❌",
+        2 => "⭕",
+        _ => null
+    };
+
+    void CardClicked((int Row, int Column) position)
+    {
+        if (computersTurn is false && matrix[position.Row, position.Column] is 0)
+        {
+            matrix[position.Row, position.Column] = isUserX ? 1 : 2;
+            computersTurn = true;
+
+            ComputerMoves().ConfigureAwait(false);
+
+            StateHasChanged();
+            if(isGameFinished()) 
+                DecideWinner();
+        }
+    }
+
+    async Task ComputerMoves()
+    {
+        await Task.Delay(1000);
+
+        Console.WriteLine("Computer pressing");
+        var emptyCards = new List<(int Row, int Column)>();
+
+        for(int row = 0; row < 3; row++)
+            for(int column = 0; column < 3; column++)
+                if (matrix[row, column] is 0)
+                    emptyCards.Add((row, column));
+
+        if(emptyCards.Any())
+        {
+            var randomCard = emptyCards.OrderBy(_ => Guid.NewGuid()).First();
+            matrix[randomCard.Row, randomCard.Column] = isUserX ? 2 : 1;
+        }
+        computersTurn = false;
+        StateHasChanged();
+
+        if(isGameFinished()) 
+            DecideWinner();
+    }
+
+    bool isGameFinished() 
+    {
+        var emptyCards = new List<(int Row, int Column)>();
+
+        for(int row = 0; row < 3; row++)
+            for(int column = 0; column < 3; column++)
+                if (matrix[row, column] is 0)
+                    emptyCards.Add((row, column));
+        
+        return emptyCards.Any() is false;
+    }
+
+    void DecideWinner()
+    {
+    }
+}
